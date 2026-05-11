dedad82ea2ce94eee20d9948f53927594ea611dc
Uday <suday7807@gmail.com>
2026-05-12 00:23:05 +0530

    Use deployed backend URL

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AppContext = createContext();
 
-const API_URL = '/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://tapcounter-backend.onrender.com';
 
 const getGuestId = () => {
   let guestId = localStorage.getItem('guestId');
